f161d313bdb6d2e4f2d9342d193d35c82901ebe3
otmangx <otmangx@gmail.com>
2019-11-16 22:54:39 +0100

    Add separate facedetect.h

---
+++ facedetect.h
@@ -0,0 +1,32 @@
+#ifndef FACEDETECT_H_INCLUDED
+#define FACEDETECT_H_INCLUDED
+using namespace std;
+using namespace cv;
+
+String face_cascade_name = "haarcascades/haarcascade_frontalface_alt.xml";
+CascadeClassifier face_cascade;
+
+void detectAndDisplay(Mat frame,String shape)
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        if (shape.compare("circle")==0) {
+            Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+            ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        } else if (shape.compare("rect")==0) {
+            Point pt1(faces[i].x, faces[i].y); // Display detected faces on main window - livestream from camera
+            Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
+            rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+        }
+        Mat faceROI = frame_gray( faces[i] );
+    }
+}
+
+
+#endif // FACEDETECT_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "facedetect.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
 using namespace cv;
-void detectAndDisplay(Mat frame, String shape="circle");
-String face_cascade_name = "haarcascades/haarcascade_frontalface_alt.xml";
-CascadeClassifier face_cascade;
+//void detectAndDisplay(Mat frame, String shape="circle");
+
 String window_name = "Capture - Face detection";
 int main( void )
 {
@@ -24,6 +24,8 @@ int main( void )
         // Read the image file
         Mat frame = imread("lena.tif");
         detectAndDisplay( frame, "rect" );
+        //-- Show what you got
+        imshow( window_name, frame );
         char c = (char)waitKey(0);
         return 0;
     } else if (mode==2) {
@@ -41,6 +43,8 @@ int main( void )
             }
             //-- 3. Apply the classifier to the frame
             detectAndDisplay( frame, "circle" );
+            //-- Show what you got
+            imshow( window_name, frame );
             char c = (char)waitKey(10);
             if( c == 27 ) { break; } // escape
         }
@@ -49,26 +53,4 @@ int main( void )
 
     return 0;
 }
-void detectAndDisplay(Mat frame,String shape)
-{
-    std::vector<Rect> faces;
-    Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-    //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        if (shape.compare("circle")==0) {
-            Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-            ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-        } else if (shape.compare("rect")==0) {
-            Point pt1(faces[i].x, faces[i].y); // Display detected faces on main window - livestream from camera
-            Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
-            rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
-        }
-        Mat faceROI = frame_gray( faces[i] );
-    }
-    //-- Show what you got
-    imshow( window_name, frame );
-}
+
