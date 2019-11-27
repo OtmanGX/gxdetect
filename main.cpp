15960f229303f1d29640becb1b91ddce83c6d53a
issam-sudo <issambenkaroum95@gmail.com>
2019-11-27 23:18:45 +0100

    les fichiers commite

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 #include <stdio.h>
 
 String window_name = "Capture - Face detection";
+
 int main( void )
 {
+int filenumber; // Number of file to be saved
+string filename;
     //-- 1. Load the cascades
     if( !face_cascade.load( face_cascade_name ) ){
         printf("--(!)Error loading face cascade\n");
@@ -26,9 +29,86 @@ int main( void )
                 exit(1);
             }
         detectAndDisplay( frame, "rect" );
+
+          /* Set Region of Interest */
+std::vector<Rect> faces;
+    Mat frame_gray;
+
+    Mat res;
+    Mat gray;
+    string text;
+    stringstream sstm;
+
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
+
+    // Detect faces
+    face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+    // Set Region of Interest
+    cv::Rect roi_b;
+    cv::Rect roi_c;
+
+    size_t ic = 0; // ic is index of current element
+    int ac = 0; // ac is area of current element
+
+    size_t ib = 0; // ib is index of biggest element
+    int ab = 0; // ab is area of biggest element
+
+    for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
+
+    {
+        roi_c.x = faces[ic].x;
+        roi_c.y = faces[ic].y;
+        roi_c.width = (faces[ic].width);
+        roi_c.height = (faces[ic].height);
+
+        ac = roi_c.width * roi_c.height; // Get the area of current element (detected face)
+
+        roi_b.x = faces[ib].x;
+        roi_b.y = faces[ib].y;
+        roi_b.width = (faces[ib].width);
+        roi_b.height = (faces[ib].height);
+
+        ab = roi_b.width * roi_b.height; // Get the area of biggest element, at beginning it is same as "current" element
+
+        if (ac > ab)
+        {
+            ib = ic;
+            roi_b.x = faces[ib].x;
+            roi_b.y = faces[ib].y;
+            roi_b.width = (faces[ib].width);
+            roi_b.height = (faces[ib].height);
+        }
+
+    }
+
+
+// Form a filename
+
+        filename = "";
+        stringstream ssfn;
+        ssfn << filenumber << ".jpg";
+        filename = ssfn.str();
+        filenumber++;
+
+        imwrite(filename, gray);
+
+        Point pt1(faces[ic].x, faces[ic].y); // Display detected faces on main window
+        Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
+        rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+
+    /* Crop the original image to the defined ROI */
+
+    cv::Mat crop = frame(roi_b);
+    cv::imshow("crop", crop);
+
+        cv::waitKey(0);
+
+   // cv::imwrite("noises_cropped.png", crop);
         //-- Show what you got
-        imshow( window_name, frame );
-        char c = (char)waitKey(0);
+    //    imshow( window_name, frame );
+
         return 0;
     } else if (mode==2) {
         //-- 2. Read the video stream
